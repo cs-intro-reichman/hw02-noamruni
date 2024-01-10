166803a081876582c426a129dde350b4baa04ca7
noamruni <noam.adda@runi.ac.il>
2024-01-10 23:11:08 +0200

    This is Noam Adda's code for HW2

--- Reverse.java
+++ Reverse.java
@@ -9,19 +9,21 @@ public class Reverse {
         int length = myString.length();
 
         // Iterate over all the characters from the end to the beginning and print each character
+        String reverseString = "";
         for (int i = length - 1; i >= 0; i--) {
-            System.out.print(myString.charAt(i));
+            reverseString += myString.charAt(i);
         }
+        System.out.println(reverseString);
 
         // Define the variable middle, and calculate its position based on the string length being even or odd
         int middle;
         if (length % 2 == 0) {
-            middle = (length / 2) + 1;
+            middle = (length / 2);
         } else {
             middle = (length - 1) / 2;
         }
 
         // Print the string middle character
-        System.out.println("\n" + "The middle character is " + myString.charAt(middle));
+        System.out.println("The middle character is " + reverseString.charAt(middle));
     }
 }
